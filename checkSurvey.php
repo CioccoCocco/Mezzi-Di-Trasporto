1e5766abff810070473e04579929ef5491a28fdb
Vanitas <117798375+Celenik@users.noreply.github.com>
2023-11-24 17:31:11 +0100

    checkSurvey.php

--- checkSurvey.php
+++ checkSurvey.php
@@ -2,18 +2,38 @@
     $name = $_POST["name"];
     $surname = $_POST["surname"];
     $publicTransport = $_POST["publicTransport"];
+    $error = "";
+
+    
+    if(empty($name)){
+        $error = "non ha inserito il nome";
+        header("location:survey.php?&error=$error");
+    }else if(empty($surname)){
+        $error = "non ha inserito il cognome";
+        header("location:survey.php?&error=$error");
+    }
+
 
     $lenght = strlen($name);
     for($i = "!"; $i <"/"; $i++)
     {
         for($k=0; $k < $lenght; $k++){
             if(strpos($string, $i, $k)){
-                echo "non puoi inserire le lettere speciali";
+                
+                
                 error_reporting(0);
             }
         }
     }
 
+    if($error != ""){
+        $error = "non puoi inserire le lettere speciali";
+        header("location:survey.php?&error=$error");
+    }
+
+    
+
     $myJSON = $myJSON = json_encode($name, $surname, $publicTransport);
 
+    
 ?>
