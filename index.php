2c8b30873387d0e54cfbe6443c974809fb9596d5
tredici <117776283+aurorapicco@users.noreply.github.com>
2023-12-01 12:27:25 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accedi</title>
+    <link type="text/css" rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="homepage">
+        <header id="header">
+
+            <img src="logo.jpg" alt="logo" id="logo">
+
+            <nav>
+                <p>Andrea Marino</p>
+                <p>Aurora Picco</p>
+                <p>Nicolas Natale</p>
+                <p>Gabriele Fazio</p>
+            </nav>
+
+        </header>
+        <h1>Benvenuto!</h1>
+        <p>Accedi per iniziare il sondaggio.</p>
+        <form action="checkPassord.php" method="get">
+            <label for="psw">Inserisci la password: </label>
+            <input type="password" name="psw"><br>
+            <input type="submit" value="INVIA">
+        </form>
+        <footer id="footer">
+            <p>Gruppo 3, "Mezzi di trasporto", Novembre 2023</p>
+        </footer>
+    </div>
+</body>
+</html>
---
+++ results.php
@@ -0,0 +1,33 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Risultati</title>
+    <link type="text/css" rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="results">
+        <header id="header">
+
+            <img src="logo.jpg" alt="logo" id="logo">
+
+            <nav>
+                <p>Andrea Marino</p>
+                <p>Aurora Picco</p>
+                <p>Nicolas Natale</p>
+                <p>Gabriele Fazio</p>
+            </nav>
+        </header>
+        <h1>Grazie per aver votato!</h1>
+        <h3>Ecco i risultati:</h3>
+        <footer id="footer">
+            <p>Gruppo 3, "Mezzi di trasporto", Novembre 2023</p>
+        </footer>
+    </div>
+</body>
+</html>
