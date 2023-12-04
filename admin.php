ee6f00b89a31fa3afdce197d36d4350460fce0ab
tredici <117776283+aurorapicco@users.noreply.github.com>
2023-12-04 19:13:56 +0100

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,22 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <link type="text/css" rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="admin">
+        <h1>Benvenuto nella zona dedicata all'admin!</h1>
+        <h3>Ecco le persone che hanno votato:</h3>
+        <div class="votanti">
+
+        </div>
+    </div>
+</body>
+</html>
