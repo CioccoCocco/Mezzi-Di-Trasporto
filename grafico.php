aa2ff3660d71b29f2536e2705fada39e752f7169
Vanitas <117798375+Celenik@users.noreply.github.com>
2023-12-04 15:06:49 +0100

    grafico.php

--- grafico.php
+++ grafico.php
@@ -1,36 +1,13 @@
 <?php
-include("phpgraphilib.php");
-$graph=new PHPGraphLib(500,400);
-$file = file_get_contents("check.json");
-$json = json_decode($file);
-$array;
+require_once("phpgraphlib.php");
+$json_data = file_get_contents("check.json");
+$data = json_decode($json_data, true);
 
-
-
-
-for($i = 0; $i< $json.$publicTransport.$lenght; $i++){
-    for($k = 0; $k < $json.$name.$lenght; $k++){
-        if($json.$name[$k].$publicTransport == $json.$pubpublicTransport[$i]){
-            $array[$i]++;
-        }
-    }
-}
-
-$graph->addData($array);
-
-$graph->setTitle("Transporti più utilizzati");
-
-$graph->setBars(false);
-
-$graph->setLine(true);
-
-$graph->setDataPoints(true);
-
-$graph->setDataPointColor("blue");
-
-$graph->setDataValues(true);
-
-$graph->setDataValueColor("red");
-
+$graph = new PHPGraphLib(400, 300);
+$graph->addData($data);
+$graph->setTitle("Preferenze di Mezzi di Trasporto");
+$graph->setXValuesHorizontal(true);
+$graph->setYValues(false);
+$graph->createGraph();
 
 ?>
