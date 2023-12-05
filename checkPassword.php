c646769b60b1c652d2fa03e530cc4598578d9d80
FaziusOmega <FaziusOmega@gmail.com>
2023-12-05 17:09:08 +0100

    Update checkPassword.php

--- checkPassword.php
+++ checkPassword.php
@@ -2,27 +2,27 @@
     $psw = $_GET['psw'];
     $_SESSION['login'] = "declined";
     $lenght = strlen($psw);
-    for($i = "!"; $i <"/"; $i++)
-    {
-        for($k=0; $k < $lenght; $k++){
+    // for($i = "!"; $i <"/"; $i++)
+    // {
+    //     for($k=0; $k < $lenght; $k++){
             
-            if(strpos($psw, $i, $k)){
-            $error = "non puoi inserire le lettere speciali";
-             error_reporting(0);
-            }
-        }
-    }
+    //         if(strpos($psw, $i, $k)){
+    //         $error = "non puoi inserire le lettere speciali";
+    //          error_reporting(0);
+    //         }
+    //     }
+    // }
     
-    if(isset($error)){
-        header("location:index.php?&error=$error");
-    }else{
+    // if(isset($error)){
+    //     header("location:index.php?&error=$error");
+    // }else{
         $_SESSION['login'] = "accept";
         if ($psw === "Survey") {
             header("location:survey.php");
         } else if ($psw === "Admin") {
             header("location:admin.php");
         }
-    }
+    // }
     
     
     
