50effc5851199adc63d63fb1f0a4726d52f33382
Vanitas <117798375+Celenik@users.noreply.github.com>
2023-12-05 17:44:12 +0100

    Update grafico.php

--- grafico.php
+++ grafico.php
@@ -7,4 +7,21 @@
 require_once("phpgraphlib.php");
 $json_data = file_get_contents("check.json");
 $data = json_decode($json_data, true);
+$graph = new PHPGraphLib(400, 300);
+
+// Aggiungi dati al grafico
+$graph->addData($data);
+
+// Imposta il titolo del grafico
+$graph->setTitle("Preferenze di Mezzi di Trasporto");
+
+// Imposta l'etichetta sull'asse X
+$graph->setXValuesHorizontal(true);
+
+// Imposta l'etichetta sull'asse Y
+$graph->setYValues(false);
+
+// Disegna il grafico
+$graph->createGraph();
+
 ?>
