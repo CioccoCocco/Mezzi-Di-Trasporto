65a95e5a3c39ece8f559d4e9496b74cc5e2ee765
FaziusOmega <FaziusOmega@gmail.com>
2023-12-05 16:56:47 +0100

    Update checkPassword.php

--- checkPassword.php
+++ checkPassword.php
@@ -1,6 +1,5 @@
 <?php
     $psw = $_GET['password'];
-    $psw_correct = false;
     $_SESSION['login'] = "declined";
     $lenght = strlen($psw);
     for($i = "!"; $i <"/"; $i++)
@@ -16,16 +15,13 @@
     
     if(isset($error)){
         header("location:index.php?&error=$error");
-    }
-    if ($psw_correct) {
+    }else{
         $_SESSION['login'] = "accept";
         if ($psw === "Survey") {
             header("location:survey.php");
         } else if ($psw === "Admin") {
             header("location:admin.php");
         }
-    }else {
-        header("location:index.php");
     }
     
     
